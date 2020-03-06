c2a3c3561b0e88a5653116bc3baa7c8cbb3621a8
kangxulong <857520663@qq.com>
2020-03-06 23:26:46 +0800

    导入tabbar

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar'
+}
+</script>
+
+<style scoped>
+  #tab-bar {
+    display: flex;
+    background-color: #f2f2f2;
+    height: 49px;
+
+    font-size: 14px;
+
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0 -1px 2px rgba(0, 0, 0, 0.1)
+  }
+</style>
---
+++ src/components/context/mainTabBar/MainTabBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path="/home"><!-- 如果对文字颜色不满意，可以使用activeColor属性修改，例如添加 activeColor="blue" -->
+        <img slot="item-icon" src="~assets/img/tabbar/home/home.svg" alt="首页">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-icon" src="~assets/img/tabbar/category/category.svg" alt="分类">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-icon" src="~assets/img/tabbar/cart/cart.svg" alt="购物车">
+        <img slot="item-icon-active" src="~assets/img/tabbar/cart/cart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~assets/img/tabbar/profile/profile.svg" alt="我的">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+
+  import TabBar from 'components/common/tabbar/TabBar'
+
+  import TabBarItem from 'components/common/tabbar/TarBarItem'
+
+export default {
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+  
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    margin-top: 3px
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
